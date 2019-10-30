dd337384b6df88f2ae3cd27a098c3d145d8aaf0c
amirrezaask <raskarpour@gmail.com>
2019-10-31 00:06:10 +0330

    tests now run in qemu

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use crate::qemu_exit::QemuExitCode;
 
 mod vga_buffer;
+mod qemu_exit;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!(r#"Chapool Kernel 0.0.1"#);
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
@@ -17,4 +23,20 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn cpu_test() {
+    print!("CPU");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
