2589a1ae5518732f35b70103cff251f45cb6b655
amirrezaask <raskarpour@gmail.com>
2019-10-31 00:28:10 +0330

    using port mapped io now tests write output in host console

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use crate::qemu_exit::QemuExitCode;
+use crate::qemu_exit::{QemuExitCode, exit_qemu};
 
 mod vga_buffer;
 mod qemu_exit;
+mod serial;
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -18,25 +20,35 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-/// This function is called on panic.
+#[cfg(not(test))] // not in test mode
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn cpu_test() {
-    print!("CPU");
+    serial_println!("CPU");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
